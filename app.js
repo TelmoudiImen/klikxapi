526b713b1282191229e9448600343dd7beb7977e
RaisoMos <zemzemi.aminou@gmail.com>
2017-08-10 00:21:16 +0100

    fix articles

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/klikx');
 app.use('/parametres', require('./routes/parametres'));
